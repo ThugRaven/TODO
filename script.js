15bb829e5e424abef4ad89ccf88654b6f901ab90
ThugRaven <kamilo1715@wp.pl>
2021-08-08 22:34:05 +0200

    Add selectable todo lists

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
+const listInput = document.querySelector(".list__input");
 const todoInput = document.querySelector(".todo__input");
+const listBtn = document.querySelector(".list__btn");
 const todoBtn = document.querySelector(".todo__btn");
+const todoSelectedList = document.querySelector(".todo__selected-list");
 const todoList = document.querySelector(".todo__list");
+const todosList = document.querySelector(".list__list");
+const listTemplate = document.getElementById("list__template");
 const todoTemplate = document.getElementById("todo__template");
 
-const LOCAL_STORAGE_KEY = "todo.list";
+const LOCAL_STORAGE_TODOS_KEY = "todo.list";
+const LOCAL_STORAGE_TODOS_LIST_ID_KEY = "todo.list_id";
 
-let todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+let todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TODOS_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_TODOS_LIST_ID_KEY);
 let todoAdded = false;
 
+listBtn.addEventListener("click", addList);
 todoBtn.addEventListener("click", addTodo);
+
+function createList(name) {
+	return { id: Date.now().toString(), name: name, todos: [] };
+}
 
 function createTodo(text) {
 	return { text: text, complete: false };
+}
+
+function addList(event) {
+	event.preventDefault();
+	const listValue = listInput.value;
+	if (listValue == null || listValue === "") return;
+
+	const list = createList(listValue);
+	// todoAdded = true;
+	todos.push(list);
+	save();
+	render();
+
+	listInput.value = "";
 }
 
 function addTodo(event) {
@@ -21,7 +47,8 @@ function addTodo(event) {
 
 	const todo = createTodo(todoValue);
 	todoAdded = true;
-	todos.push(todo);
+	const selectedList = todos.find((list) => list.id === selectedListId);
+	selectedList.todos.push(todo);
 	save();
 	render();
 
@@ -29,12 +56,20 @@ function addTodo(event) {
 }
 
 function save() {
-	localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+	localStorage.setItem(LOCAL_STORAGE_TODOS_KEY, JSON.stringify(todos));
+	localStorage.setItem(LOCAL_STORAGE_TODOS_LIST_ID_KEY, selectedListId);
 }
 
 function render() {
-	clearElements(todoList);
-	renderTodos();
+	clearElements(todosList);
+
+	renderLists();
+	const selectedList = todos.find((list) => list.id === selectedListId);
+
+	if (selectedListId != null) {
+		clearElements(todoList);
+		renderTodos(selectedList);
+	}
 }
 
 function clearElements(element) {
@@ -43,8 +78,32 @@ function clearElements(element) {
 	}
 }
 
-function renderTodos() {
-	todos.forEach((todo, index) => {
+function renderLists() {
+	todos.forEach((list) => {
+		const newList = document.importNode(listTemplate.content, true);
+
+		const listItem = newList.querySelector(".list");
+		listItem.dataset.id = list.id;
+		listItem.innerHTML = `${
+			list.todos.filter((todo) => todo.complete === true).length
+		}/${list.todos.length} ${list.name}`;
+
+		listItem.addEventListener("click", (event) => {
+			if (event.target.dataset.id != null) {
+				selectedListId = event.target.dataset.id;
+				save();
+				render();
+			}
+		});
+
+		todosList.appendChild(newList);
+	});
+}
+
+function renderTodos(selectedList) {
+	todoSelectedList.innerHTML = selectedList.name;
+
+	selectedList.todos.forEach((todo, index) => {
 		const newTodo = document.importNode(todoTemplate.content, true);
 
 		const todoText = newTodo.querySelector(".todo__text");
@@ -57,8 +116,8 @@ function renderTodos() {
 		const todoLabel = newTodo.querySelector("label");
 		todoLabel.htmlFor = "todo-" + index;
 
-		todoCheckbox.addEventListener("change", (event) => {
-			todos[index].complete = !todos[index].complete;
+		todoCheckbox.addEventListener("change", () => {
+			selectedList.todos[index].complete = !selectedList.todos[index].complete;
 			save();
 		});
 
@@ -69,14 +128,14 @@ function renderTodos() {
 			todoLi.classList.add("fade-out");
 			todoLi.addEventListener("animationend", (event) => {
 				if (event.animationName === "fadeOut") {
-					todos.splice(index, 1);
+					selectedList.todos.splice(index, 1);
 					save();
 					render();
 				}
 			});
 		});
 
-		if (todoAdded && index === todos.length - 1) {
+		if (todoAdded && index === selectedList.todos.length - 1) {
 			todoLi.classList.add("fade-in");
 			todoLi.addEventListener("animationend", (event) => {
 				todoLi.classList.remove("fade-in");
